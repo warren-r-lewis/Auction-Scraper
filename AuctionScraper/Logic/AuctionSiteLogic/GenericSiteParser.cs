e4ea751518a28c160c2120bcc766b5228d727aec
Warren Lewis <alphadube@Warrens-MacBook-Pro.local>
2021-12-23 18:46:15 -0800

    added article logic

--- AuctionScraper/Logic/AuctionSiteLogic/GenericSiteParser.cs
+++ AuctionScraper/Logic/AuctionSiteLogic/GenericSiteParser.cs
@@ -12,7 +12,34 @@ namespace AuctionScraper.Logic.AuctionSiteLogic
             return await response;
         }
 
-		public GenericSiteParser()
+        public static List<GenericAuction> RetrieveNews()
+        {
+            var nytimes = "https://www.nytimes.com/";
+            var ny_response = CallUrl(nytimes).Result;
+            HtmlDocument ny_htmlDocument = new HtmlDocument();
+            ny_htmlDocument.LoadHtml(ny_response);
+            var Nodes = new List<HtmlNode>();
+            Nodes = ny_htmlDocument.DocumentNode.SelectNodes("//h3").ToList(); //.Where(x=>x.InnerHtml.Contains("story-wrapper")).ToList();
+            var auctionItems = new List<GenericAuction>();
+            foreach (var node in Nodes)
+            {
+                string title = node.InnerText;
+                string url = "No Url";
+                var urlParent = node.ParentNode.Ancestors();
+                foreach (var urlNode in urlParent)
+                {
+                    if (urlNode.Name == "a")
+                    {
+                        url = urlNode.Attributes["href"].Value;
+                    }
+                }
+
+                var item = new GenericAuction();
+                auctionItems.Add(item);
+            }
+            return auctionItems;
+        }
+        public GenericSiteParser()
 		{
 		}
 	}
